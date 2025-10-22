5b2b15cce0b51efaec27a3243715b6735e5754f8
odelattregh <106011574+odelattregh@users.noreply.github.com>
2025-10-22 12:40:38 +0200

    enable lint for the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginFailedComponent } from './status/login-failed';
 import { LougoutFailedComponent } from './status/logout-failed';
 import { LougoutSuccessComponent } from './status/logout-success';
-import { HomeComponent } from './home/home';
 import { ProfileComponent } from './profile/profile';
 import { MsalGuard } from '@azure/msal-angular';
 
 export const routes: Routes = [
   { path: '', title: 'Home', loadComponent: () => import('./home/home').then(m => m.HomeComponent), canActivate: [MsalGuard], pathMatch: 'full' },
   { path: 'profile', component: ProfileComponent, canActivate: [MsalGuard] },
-  { path: '', component: HomeComponent, canActivate: [MsalGuard] },
   { path: 'login-failed', component: LoginFailedComponent },
   { path: 'logout-failed', component: LougoutFailedComponent },
   { path: 'logout-success', component: LougoutSuccessComponent },
